b356da37f956612958f351b2bbaa16dd2d8a5b27
Cdm2883 <Cdm2883@users.noreply.github.com>
2025-09-14 07:49:38 +0000

    deploy: 7ccc4614b07d1de6fb766176727a14661045564c

---
+++ preview/assets/javascripts/mathjax.js
@@ -0,0 +1,22 @@
+// noinspection JSUnresolvedReference
+
+window.MathJax = {
+    tex: {
+        inlineMath: [["\\(", "\\)"]],
+        displayMath: [["\\[", "\\]"]],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        ignoreHtmlClass: ".*|",
+        processHtmlClass: "arithmatex"
+    }
+};
+
+// noinspection JSIgnoredPromiseFromCall
+document$.subscribe(() => {
+    MathJax.startup.output.clearCache()
+    MathJax.typesetClear()
+    MathJax.texReset()
+    MathJax.typesetPromise()
+})
